823560edd29777a8e0a515c50451c974cdccb2fa
faizur <faizursherasiyaai@gmail.com>
2024-10-14 10:48:04 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(5000);
 }
 bootstrap();
